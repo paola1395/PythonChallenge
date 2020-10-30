0d03d77ab3ff46aaedf21a81fe16a11d4a33bb5f
Paola Lopez <paola.lopez1@yahoo.com>
2020-10-30 13:27:06 -0700

    Update files

---
+++ PyBank/Main_Bank.py
@@ -0,0 +1,58 @@
+import os
+import csv
+
+#Read and import csv from Resources folder
+PyBank_budget_data=os.path.join("Resources/PyBank_budget_data.csv")
+month_count =0
+total=0
+max_value= -999999999
+max_month= ""
+min_value= 999999999
+min_month= ""
+total_change= []
+last_profit=0
+
+def average(lst): 
+    return sum(lst) / len(lst) 
+
+with open (PyBank_budget_data, 'r') as csv_file:
+    
+    csv_reader=csv.reader(csv_file, delimiter=',')
+    csv_header=next(csv_reader)
+
+
+    for row in csv_reader:
+        profit = int(row[1])
+        month = row[0]
+
+        if month_count ==0:
+            last_profit=profit
+
+        else:
+            #print("calculate change")
+            change= profit-last_profit
+            total_change.append(change)
+
+            if min_value > change:
+                min_value=change
+                min_month=month
+
+            if max_value < change:
+                max_value=change
+                max_month=month
+
+
+
+            #update last_profit
+            last_profit=profit
+
+        total=total+profit
+        month_count += 1
+
+print("Financial Analysis")
+print("------------------------------")
+print(f'Total Months: {month_count}')
+print(f'Total: ${total: 0,.0f}')
+print(f'Average: ${average(total_change): 0,.0f}')
+print(f'Greatest Increase: {max_month}, ${max_value: 0,.0f}')
+print(f'Greatest Decrease: {min_month}, ${min_value: 0,.0f}')
